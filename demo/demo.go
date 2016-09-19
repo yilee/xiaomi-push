26a2ae865ff07c329af859aefffc376d75efc534
施凯伦 <kshi@linkedin.com>
2016-09-19 14:08:36 +0800

    init xiaomi push golang sdk

--- demo/demo.go
+++ demo/demo.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/yilee/xiaomi-push/xiaomipush"
+	xm "github.com/yilee/xiaomi-push"
 )
 
 func main() {
-	msg := xiaomipush.NewMessage("hi baby")
-	client := xiaomipush.NewClient("xxxxxsad", "packagename")
+	msg := xm.NewMessage("hi baby")
+	client := xm.NewClient("xxxxxsad", "packagename")
 	result, err := client.Push(msg, []string{"fake_reg_id_1", "fake_reg_id_2"})
 	if err != nil {
 		return
